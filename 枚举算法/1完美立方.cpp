1d7e6b94c1c44cd4477c0ba6731bb641cd3a61d0
agaojs <a@gaojs.cn>
2019-11-06 13:37:51 +0800

    简单枚举（完美立方）

---
+++ 枚举算法/1完美立方.cpp
@@ -0,0 +1,28 @@
+//a*a*a==b*b*b+c*c*c+d*d*d(b<=c<=d,n<=100) 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "input n:";
+	cin >> n; //ÊäÈëÕûÊın=24	
+	for (int a = 2; a <= n; a++)
+	{
+		for (int b = 2; b <= a; b++)
+		{
+			for (int c = b + 1; c <= a; c++)
+			{
+				for (int d = c + 1; d <= a; d++)
+				{
+					if (a * a * a == b * b * b + c * c * c + d * d * d)
+					{
+						printf("Cube = %d, Triple = (%d,%d,%d)\n", a, b, c, d);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
