466f3b078ce11d72a08659c58f45cd93c19d2d80
notebook <notebook@ni8.fun>
2019-11-30 21:23:20 +0800

    逆序数，未能在百练上验证

---
+++ 4分治算法/4排列的逆序数.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int N=100000;
+int a[N]={0};
+
+//将前k大的数，都移动到最左侧 
+int ni(int a[], int n)
+{ 	
+	if(n<=1)
+	{
+		return 0;
+	}
+	else
+	{
+		int mid=n/2, sum=0,i,j;
+		sum+=ni(a,mid);
+		sum+=ni(a+mid,n-mid);
+		sort(a,a+mid);
+		sort(a+mid,a+n);
+		//2 3 6 ; 1 4 5
+		i=0,j=mid;
+		while(i<mid && j<n)
+		{			
+			while(i<mid && a[i]<=a[j])
+			{
+				i++;			
+			}
+			while(j<n && a[i]>a[j])
+			{
+				sum+=mid-i;
+				j++;
+			}
+		}		
+		return sum;	
+	}
+}
+
+int main()
+{
+	int n,i;
+	
+	cin >> n;
+	for(i=0;i<n;i++)
+	{
+		//cin>>a[i];
+		scanf("%d",&a[i]);
+	}
+	cout<<ni(a,n);
+	return 0;
+}
