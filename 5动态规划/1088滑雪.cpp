2a858076f6036dafe0a858e919eb08476dda2951
agaojs <a@gaojs.cn>
2019-12-19 09:59:35 +0800

    竟然超时了

--- 5动态规划/1088滑雪.cpp
+++ 5动态规划/1088滑雪.cpp
@@ -3,16 +3,75 @@
 using namespace std;
 
 const int R = 100, C = 100;
+//每个区块的高度值
 int a[R][C] = { 0 };
+//以每个区块为终点的路线长度
+int len[R][C] = { 0 };
 
-int find(int** a, int r, int c)
+int find(int a[R][C], int r, int c)
 {
-	return 0;
+	int i, j, k, max = 0;
+
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			len[i][j] = 1;
+		}
+	}
+	for (k = 0; k < r * c; k++)
+	{//路线最长，无非是r*c
+		for (i = 0; i < r; i++)
+		{
+			for (j = 0; j < c; j++)
+			{
+				if (i > 0 && a[i][j] > a[i - 1][j])
+				{//可以从上面滑过来
+					if (len[i][j] < len[i - 1][j] + 1)
+					{
+						len[i][j] = len[i - 1][j] + 1;
+					}
+				}
+				if (i <r - 1 && a[i][j] > a[i + 1][j])
+				{//可以从下面滑过来
+					if (len[i][j] < len[i + 1][j] + 1)
+					{
+						len[i][j] = len[i + 1][j] + 1;
+					}
+				}
+				if (j > 0 && a[i][j] > a[i][j - 1])
+				{//可以从左面滑过来
+					if (len[i][j] < len[i][j - 1] + 1)
+					{
+						len[i][j] = len[i][j - 1] + 1;
+					}
+				}
+				if (j <c - 1 && a[i][j] > a[i][j + 1])
+				{//可以从下面滑过来
+					if (len[i][j] < len[i][j + 1] + 1)
+					{
+						len[i][j] = len[i][j + 1] + 1;
+					}
+				}
+			}
+		}
+	}
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+		{
+			if (max < len[i][j])
+			{
+				max = len[i][j];
+			}
+		}
+	}
+	return max;
 }
 
 int main()
 {
-	int r, c,i,j;
+	int r, c, i, j;
 
 	cin >> r >> c;
 	for (i = 0; i < r; i++)
