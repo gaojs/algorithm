032c21a6e3a3924f2182cb877e6d9b5f090c1ff5
g30000454 <gaojinshan@huawei.com>
2020-05-07 14:31:16 +0800

    leetcode/128. 最长连续序列.cpp。不符合要求（O(n)），也能通过

--- leetcode/128. 最长连续序列.cpp
+++ leetcode/128. 最长连续序列.cpp
@@ -3,28 +3,42 @@
 #include <string.h>
 #include <math.h>
 
-#define COUNT  100
-int a[COUNT][COUNT] = {0}; // i到j的最大利润 
-int m[COUNT] = {0}; // 前i项最小值 
-int maxProfit(int* prices, int size)
+int cmp(const void*a, const void*b)
 {
-	if ( prices == NULL || size == 0) {
-		return 0;
-	}
-	memset(a, 0, sizeof(a));
-	m[0] = prices[0]; // 第一项 
-	for (int i = 1; i < size; i++) {
-		
-	}
-	  
+    return (long long)*(int*)a - (long long)*(int*)b;
+}
+
+int longestConsecutive(int* a, int n)
+{
+    if (a == NULL || n == 0) {
+        return 0;
+    }
+    qsort(a, n, sizeof(int), cmp);    
+    int t =a[0], cnt = 1, max = 1;
+    for (int i = 1; i < n; i++) {
+        if (a[i] == t) {
+            continue; 
+        } else if (a[i] - t == 1) {
+            t = a[i]; 
+            cnt++;
+            if (max < cnt) {
+                max = cnt; 
+            }
+        } else {
+            t = a[i]; 
+            cnt = 1;
+        }            
+    }
+    return max;
 }
 
 int main()
 {
-	//int a[] = {}; // 0表示null 
-	int a[] = {3,3,5,0,0,3,1,4};
-	int size = sizeof(a) / sizeof(a[0]);
-	printf("%d", maxProfit(a, size));		
+    // int a[] = {1,2,0,1}; // 3
+	// int a[] = {100, 4, 200, 1, 3, 2}; // 4
+	int a[] = {2147483646,-2147483647,0,2,2147483644,-2147483645,2147483645}; // 4
+	int n = sizeof(a) / sizeof(a[0]);
+	printf("%d", longestConsecutive(a, n));		
 	return 0;
 }
 
