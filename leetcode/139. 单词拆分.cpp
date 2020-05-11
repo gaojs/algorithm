630171af2721c79cb4f419f03501cee7cbb6709c
g30000454 <gaojinshan@huawei.com>
2020-05-11 12:39:10 +0800

    动态规划，AC

--- leetcode/139. 单词拆分.cpp
+++ leetcode/139. 单词拆分.cpp
@@ -3,67 +3,56 @@
 #include <string.h>
 #include <limits.h>
 
-bool dfs(char *s, char **wd, int n)
-{
-	if (s == NULL || wd == NULL || n == 0) {
-		return false;
-	} else if (s[0] == 0) {
-		return true;
-	}
-	for (int i = 0; i < n; i++) {
-		int len = strlen(wd[i]);
-		if (strncmp(s, wd[i], len) == 0) {
-			if (dfs(s + len, wd, n)) {
-				return true;
-			}
-		}
-	} 
-	return false;
-}
-
 bool wordBreak(char *s, char **wd, int n)
 {
 	if (s == NULL || wd == NULL || n == 0) {
+		return false; // 参数错误
+	}
+	int len = strlen(s);
+	// dp[i] = dynamic plan, 前i个字符串，是否OK
+	bool *dp = (bool *)calloc(sizeof(bool), len + 1);
+	if (dp == NULL) { // 内存申请失败
 		return false;
 	}
-	const int count = 128; 
-	int sMap[count];// = {0};
-	int sLen = strlen(s);
-	memset(sMap, 0, sizeof(sMap));
-	for (int i = 0; i < sLen; i++) {
-		sMap[s[i]] = 1; // s中有哪些字母 
-	}
-	int wdMap[count];// = {0};
-	memset(wdMap, 0, sizeof(wdMap));
-	for (int i = 0; i < n; i++) {
-		int len = strlen(wd[i]);
-		for (int j = 0; j < len; j++) {
-			wdMap[wd[i][j]] = 1; // wd字母 
+	dp[0] = true; // 前0个字符串，认为OK
+	for (int i = 1; i <= len; i++) {
+		for (int j = 0; j < n; j++) {
+			int wdlen = strlen(wd[j]);
+			int k = i - wdlen; // 前段
+			if (k < 0) { //不够长
+				continue;
+			}
+			dp[i] = (dp[k] && strncmp(s + k, wd[j], wdlen) == 0) || dp[i];
+			/*if (dp[k] && strncmp(s + k, wd[j], wdlen) == 0) {
+				printf("i=%d, k=%d,  s=%s, wd[%d]=%s, wdlen=%d,  dp[%d]=%d, dp[%d]=%d\n",
+					i, k, s + k, j, wd[j], wdlen, k, dp[k], i, dp[i]);
+			}*/
 		}
 	}
-	for (int i = 0; i < count; i++) {
-		if (sMap[i] > wdMap[i]) {
-			printf("%d: %d, %d\n", i, sMap[i], wdMap[i]);
-			return false; // 有些字母在字典中不存在 
-		}
-	}	
-	return dfs(s, wd, n);
+	/*for (int i = 1; i <= len; i++) {
+		printf("%d:%d ", i, dp[i]);
+	}
+	printf("\n");
+	*/
+	bool ret = dp[len];
+	free(dp); // 释放内存
+	return ret;
 }
 
 int main()
 {
-	const char s[] = "leetcode", *wd[] = {"leet", "code"}; // true
-	// char s[] = "applepenapple", *wd[] = {"apple", "pen"}; // true
-	// char s[] = "catsandog", *wd[] = {"cats", "dog", "sand", "and", "cat"}; // false
-	/* char s[] = "bccdbacdbdacddabbaaaadababadad", 
+	// const char s[] = "leetcode", *wd[] = {"leet", "code"}; // true
+	// const char s[] = "applepenapple", *wd[] = {"apple", "pen"}; // true
+	// const char s[] = "catsandog", *wd[] = {"cats", "dog", "sand", "and", "cat"}; // false
+	/* const char s[] = "bccdbacdbdacddabbaaaadababadad",
 		*wd[] = {"cbc","bcda","adb","ddca","bad","bbb","dad","dac","ba","aa",
 			"bd","abab","bb","dbda","cb","caccc","d","dd","aadb","cc","b",
 			"bcc","bcd","cd","cbca","bbd","ddd","dabb","ab","acd","a",
 			"bbcc","cdcbd","cada","dbca","ac","abacd","cba","cdb",
 			"dbac","aada","cdcda","cdc","dbc","dbcb","bdb","ddbdd",
 			"cadaa","ddbc","babb"}; */// true
-	// char s[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
-	//	*wd[] = {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
+	const char s[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+		*wd[] = {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
 	int n = sizeof(wd) / sizeof(wd[0]);
 	
 	/*int r = strcmp(wd[0], wd[1]);
