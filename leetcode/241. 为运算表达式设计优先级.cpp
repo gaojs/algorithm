59f9c8a57c7ee0bf40b29fef34af79cbf5705d18
g30000454 <gaojinshan@huawei.com>
2020-05-22 20:11:44 +0800

    递归，分治，解决

--- leetcode/241. 为运算表达式设计优先级.cpp
+++ leetcode/241. 为运算表达式设计优先级.cpp
@@ -1,93 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define N 100
-int num[N] = { 0 };
-int op[N] = { 0 };
-
-bool dfs(int num[], int op[], int *result, int* returnSize)
+#define MAX_COUNT 10000 // 确保足够大
+// Note: The returned array must be malloced, assume caller calls free().
+int* diffWaysToCompute(char *str, int* returnSize)
 {
-    if (num == NULL || op == NULL || returnSize == NULL) {
-        return false;
-    }
-    if (op[0] == 0 && num[0] == 1) { // 找到一个答案
-        result[*returnSize] = num[1];
-        (*returnSize)++;
-    }
-    for (int i = 1; i <= op[0]; i++) {
-        int size = sizeof(int)*(num[0] + 1);
-        int *nt = (int*)malloc(size);
-        memcpy(nt, num, size);
-        int size2 = sizeof(int)*(op[0] + 1);
-        int *ot = (int*)malloc(size2);
-        memcpy(ot, op, size2);
-        switch (op[i]) {
-        case '+':
-            nt[i] += nt[i + 1];
-            break;
-        case '-':
-            nt[i] -= nt[i + 1];
-            break;
-        case '*':
-            nt[i] *= nt[i + 1];
-            break;
-        }
-        if (nt[0] > (i + 2)) {
-            memcpy(&nt[i + 1], &nt[i + 2], sizeof(int)*(nt[0] - (i + 2)));
-        }
-        if (ot[0] > (i + 1)) {
-            memcpy(&ot[i], &ot[i + 1], sizeof(int)*(ot[0] - (i + 1)));
-        }
-        nt[0]--;
-        ot[0]--;
-        dfs(nt, ot, result, returnSize);
-        free(nt);
-        free(ot);
-    }
-    return true;
-}
-
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* diffWaysToCompute(char *input, int* returnSize)
-{
-    if (input == NULL || returnSize == NULL) {
+    if (str == NULL || returnSize == NULL) {
         return NULL;
     }
     *returnSize = 0;
-    memset(num, 0, sizeof(num));
-    memset(op, 0, sizeof(op));
-    while (input[0] != 0) {
-        if (input[0] >= '0' && input[0] <= '9') {
-            // 读取一个数
-            num[0]++; // 个数
-            num[num[0]] = atoi(input);
-            while (input[0] >= '0' && input[0] <= '9') {
-                input++; // 略过这个数
+    int ops = 0, len = strlen(str);
+    for (int i = 0; i < len; i++) { // 求运算符个数
+        if (str[i] >= '0' && str[i] <= '9') {
+            continue; // 正常的数字
+        } else if (str[i] == '+' || str[i] == '-' || str[i] == '*') {
+            ops++; // 符号数
+        } else {
+            return NULL;
+        }
+    }
+    if (ops == 0) { // 没有运算符，只有一个数
+        int *result = (int*)malloc(sizeof(int));
+        result[0] = atoi(str);
+        *returnSize = 1;
+        return result;
+    } else { // 对每个运算符，进行分治处理
+        int size = sizeof(int)* MAX_COUNT;
+        int *result = (int*)malloc(size);
+        if (result == NULL) {
+            return NULL;
+        }
+        memset(result, 0, size);
+        for (int i = 0; i < len; i++) {
+            if (str[i] == '+' || str[i] == '-' || str[i] == '*') {
+                char *leftStr = (char *)calloc(sizeof(char), i + 1);
+                char *rightStr = (char *)calloc(sizeof(char), len - i);
+                strncpy(leftStr, str, i);
+                strncpy(rightStr, str + i + 1, len - i - 1);
+                int leftSize, rightSize;
+                int *leftArr = diffWaysToCompute(leftStr, &leftSize);
+                int *rightArr = diffWaysToCompute(rightStr, &rightSize);
+                for (int left = 0; left < leftSize; left++) {
+                    for (int right = 0; right < rightSize; right++) {
+                        int a = leftArr[left];
+                        int b = rightArr[right];
+                        switch (str[i]) {
+                        case '+':
+                            result[*returnSize] = a + b;
+                            break;
+                        case '-':
+                            result[*returnSize] = a - b;
+                            break;
+                        case '*':
+                            result[*returnSize] = a * b;
+                            break;
+                        }
+                        if ((*returnSize) < MAX_COUNT) {
+                            (*returnSize)++;
+                        } else {
+                            printf("overflow\n");
+                            break;
+                        }
+                    }
+                }
+                free(leftStr);
+                free(rightStr);
+                free(leftArr);
+                free(rightArr);
             }
         }
-        switch (input[0]) {
-        case '+':
-        case '-':
-        case '*':
-            op[0]++;
-            op[op[0]] = input[0];
-            input++; // 略过这个符号
-            break;
-        default:
-            break;
-        }
+        return result;
     }
-    int size = sizeof(int) * (op[0] + 1) * (op[0] + 1);
-    int *result = (int*)malloc(size);
-    if (result == NULL) {
-        return NULL;
-    }
-    memset(result, 0, size);
-    dfs(num, op, result, returnSize);
-    return result;
 }
 
 int main()
@@ -95,7 +78,8 @@ int main()
     /*****************************
     */
     // char *str = (char*)"2-1-1"; // [0,2]
-    char *str = (char*)"2*3-4*5"; // [-34,-14,-10,-10,10]
+    // char *str = (char*)"2*3-4*5"; // [-34,-14,-10,-10,10]
+    char *str = (char*)"1-2+3*4-5*6-7+8*9";
     int returnSize = 0;
     int *p = diffWaysToCompute(str, &returnSize);
     if (p != NULL) {
