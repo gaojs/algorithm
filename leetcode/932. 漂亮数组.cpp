60b3817b5f24b05a0dfb3a3338a87b23455f345b
gaojs <a@gaojs.cn>
2020-05-24 07:19:23 +0800

    分治思想，奇左偶右

--- leetcode/932. 漂亮数组.cpp
+++ leetcode/932. 漂亮数组.cpp
@@ -2,44 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 测试t是否能插入到数组a的m的位置 
-bool ok(int *a, int n, int m, int t) 
+// 利用分治思想，找出一种放法，保证符合题意。
+// a是个数组，n是数字元素的个数；奇数放左边，偶数放右边。
+// 将n1开始，步长为step的数字，放到p1到p2的连续的位置上。
+void dfs(int *a, int n, int p1, int p2, int n1, int step) 
 {
-	if (a == NULL || n <= 0) {
-		return false;
-	}
-	for (int i = 0; i < m; i++) { // 前 
-		for (int j = i + 1; j < m; j++) {
-			if(a[i] + t == a[j] * 2) {
-				return false;
-			}
-		}
+	if (a == NULL || n <= 0 || p1 >=n || p2 >=n || p1 > p2) {
+		return; // 参数不对 
+	} else if (p1 == p2) { // 只有1个元素 
+		a[p1] = n1;
+	} else { // 3个元素及以上
+		int p = (p1 + p2) / 2; // 中间位置 
+		dfs(a, n, p1, p, n1, step * 2); // 左边
+		dfs(a, n, p + 1, p2, n1 + step, step * 2); // 右边
 	} 
-	for (int i = m; i < n; i++) { // 后 
-		for (int j = i + 1; j < n; j++) {
-			if(a[j] + t == a[i] * 2) {
-				return false;
-			}
-		}
-	} 
-	return true;
-}
-
-// 将t插入到数组a的m的位置 
-int* insert(int *a, int n, int m, int t) 
-{
-	int *p = (int*)calloc(n + 1, sizeof(int));
-	if (p == NULL) {
-		return NULL;
-	}
-	if (m > 0) { // 前面 
-		memcpy(p, a, sizeof(int) * m);
-	}
-	p[m] = t; // 插入位置
-	if (n > m) { // 后面 
-		memcpy(&p[m + 1], &a[m], sizeof(int) * (n-m));
-	}
-	return p;
 } 
 
 int* beautifulArray(int n, int* returnSize)
@@ -47,41 +23,22 @@ int* beautifulArray(int n, int* returnSize)
 	if (n < 1 || returnSize == NULL) {
 		return NULL;
 	} else {
-		if(n == 1) {
-			int *a = (int*)calloc(1, sizeof(int));
-			if (a == NULL) { // 申请1个空间 			
-				*returnSize = 0;
-				return NULL;
-			}
-			*returnSize = a[0] = 1;
-			return a;			
-		} else {
-			int size = 0;
-			int *a = beautifulArray(n - 1, &size);
-			if (a == NULL || size == 0) {			
-				*returnSize = 0;
-				return NULL;
-			}
-			for (int i = n - 1; i >= 0; i--) {
-				if (ok(a, n - 1, i, n)) {
-					int *p = insert(a, n - 1, i, n);
-					if (p != NULL) { // 找到1个 
-						free(a); // 释放a 					
-						*returnSize = n;
-						return p;
-					}
-				}
-			}
-			free(a); // 释放a 
+		int size = sizeof(int) * n;
+		int *a = (int*)malloc(size);			
+		if (a == NULL) {			
 			*returnSize = 0;
 			return NULL;
-		} 		
+		}
+		dfs(a, n, 0, n - 1, 1, 1);
+		*returnSize = n;
+		return a;
 	}
 }
 
 int main()
 {
-	for (int n = 1; n < 101; n++) {		
+	//for (int n = 1; n < 20; n++) {	
+		{ int n = 4;
 		int returnSize = 0;
 		int *a = beautifulArray(n, &returnSize);
 		if (a != NULL) {
