174173be88e3d9e8de2821e5048e4a96a28036a1
gaojs <a@gaojs.cn>
2020-05-23 11:45:08 +0800

    更好的方法，大了再比较

--- leetcode/1163. 按字典序排在最后的子串.cpp
+++ leetcode/1163. 按字典序排在最后的子串.cpp
@@ -8,22 +8,16 @@ char *lastSubstring(char *s)
 		return NULL;
 	} else {
 		int len = strlen(s);
-		if (len <= 1) {
-			return s;
-		}
-		char *last = &s[len - 1];
-		bool equal = true; // 默认是相等 
-		for (char *p = last - 1; p >= s; p--) {
-			if(equal && *p == *last) { // 字符相等 
-				last = p; // 往前移动 
-			} else {
-				equal = false; 
-				if(strcmp(p, last) > 0) {
-					last = p;					
-				}
-			}
-		}
-		return last;
+	    int ans = 0;
+	    for (int i = 1; i < len; i++) {
+	        if (s[i] <= s[i - 1]) {
+	            continue;
+	        }
+	        if (strcmp(&s[i], &s[ans]) > 0) {
+	            ans = i;
+	        }
+	    }
+	    return &s[ans];
 	}
 }
 
