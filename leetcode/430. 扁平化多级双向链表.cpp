ffa5ead3b5740875b1ba3922807f1a4a28cddc53
gaojs <a@gaojs.cn>
2020-05-25 00:45:30 +0800

    待解决

---
+++ leetcode/430. 扁平化多级双向链表.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+char *crackSafe(int n, int k)
+{
+	return NULL;
+}
+
+int main()
+{
+	int n = 1, k = 2; // 01
+	int n = 2, k = 2; // 00110 10011 11001
+	printf("%s", crackSafe(n,k));
+	return 0;
+} 
+
+
