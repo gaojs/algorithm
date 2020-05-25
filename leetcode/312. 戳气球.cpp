042d77768ee4975715d9a1633fbff0a8769dfcab
g30000454 <gaojinshan@huawei.com>
2020-05-25 12:50:12 +0800

    动态规划，搞定

--- leetcode/312. 戳气球.cpp
+++ leetcode/312. 戳气球.cpp
@@ -3,77 +3,35 @@
 #include <string.h>
 #include <math.h>
 
-
-int findPos(int *a, int n)
+// dp(i, j)表示戳破第i个气球和第j个气球之间的气球，能获得的最大数量。注意不包括第i个气球和第j个气球。
+// 假设i，j之间最后一个被戳破的气球是k，则dp(i,j) = max(dp(i,k) + a[i] * a[k] * a[j] + dp(k,j)) i < k < j;
+// a[i] * a[k] * a[j] 表示最后一次戳破时的获得的最大数量，开头和结尾添加两个无法戳破的虚拟气球。
+// dp(i,k)是在戳破k之前，戳破i和k之间的气球获得收益；dp(k,j)是在戳破k之前，戳破k和j之间的气球获得收益；
+int maxCoins(int*nums, int numsSize)
 {
-	if (n <= 1) { // 只有1个，就它了
-		return 0;
-	} else if (n == 2) { // 有2个，选小的
-		return a[0] < a[1] ? 0 : 1;
-	}
-	int min = 1; // 大于3个，选择中间的最小的
-	for (int i = 2; i < n - 1; i++) {
-		if (a[min] < a[i]) {
-			continue;
-		} else if (a[min] > a[i]) {
-			min = i;
-		} else {
-			int mins = a[min - 1] * a[min + 1];
-			int is = a[i - 1] * a[i + 1];
-			if (mins < is) { // 选个左右乘积较大的
-				min = i;
-			}
-		}
-	}
-	return min;
-}
-
-int calcOne(int *a, int n, int pos)
-{
-	int left = 1, right = 1;
-	if (pos > 0) {
-		left = a[pos - 1];
-	}
-    if (pos < n - 1) {
-    	right = a[pos + 1];
-	}
-	return left * a[pos] * right;
-}
-
-int removeOne(int *a, int n, int pos)
-{
-	if (n - pos - 1 > 0) {
-		memmove(&a[pos], &a[pos + 1], sizeof(int) * (n - pos - 1));
-	}
-	a[n - 1] = 0;
-	return 0;
-}
-
-void print(int *a, int n) {
-	for (int i = 0; i < n; i++) {
-		printf("%d ", a[i]);
-	}
-	printf("\n");
-}
-
-int maxCoins(int*a, int n)
-{
-	if (a == NULL || n <= 0) {
+	if (nums == NULL || numsSize <= 0) {
 		return 0;
 	}
-	print(a, n);
-	int sum = 0;
-	while (n > 0) {
-		int pos = findPos(a, n);
-		printf("pos = %d, ", pos);
-		int t = calcOne(a, n, pos);
-		removeOne(a, n, pos);
-		sum += t;
-		printf("num = %d, sum = %d\n", t, sum);
-		n--;
-		print(a, n);
-	}
-	return sum;
+	int a[numsSize + 2];
+	int n = sizeof(a) / sizeof(a[0]);
+	a[0] = a[n-1] = 1; // 首位各加个1
+	memcpy(&a[1], nums, sizeof(int)*numsSize);
+	int dp[n][n]; // = {0}; // dp[i][j]是从i到j的最大值
+	memset(dp, 0, sizeof(dp));
+
+	for (int s = 2; s < n; s++) { // 步长
+    	for(int i = 0; i < n - s; i++) { // 从前往后
+    		int j = i + s; // 最右端；先计算短步长，更好理解
+            for(int k = i + 1; k < j; k++) { // 方向任意
+            	// 转化方程，因为dp[i][j]已经被初始化为0了
+            	int t = dp[i][k] + dp[k][j] + a[i] * a[k] * a[j];
+				if (dp[i][j] < t) { // 找最大的
+					dp[i][j] = t;
+				}
+            }
+        }
+    }
+    return dp[0][n-1];
 }
 
 int main()
