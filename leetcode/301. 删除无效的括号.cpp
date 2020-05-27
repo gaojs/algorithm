6647a843998fb60b456b489115194ee691bcc2b2
g30000454 <gaojinshan@huawei.com>
2020-05-27 15:34:37 +0800

    难，官方题解用回溯，以后再解，汗！

--- leetcode/301. 删除无效的括号.cpp
+++ leetcode/301. 删除无效的括号.cpp
@@ -4,29 +4,66 @@
 #include <ctype.h>
 #include <math.h>
 
+
+#define MAX_COUNT 100
+
+bool isValid(char *s)
+{
+    if (s == NULL) {
+        return false;
+    }
+    int left, right;
+    left = right = 0;
+    for (; s[0]; s++) {
+        switch (s[0]) {
+        case '(':
+            left++;
+            break;
+        case ')':
+            right++;
+            if (right > left) {
+                return false;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    return (left == right);
+}
+
 char **removeInvalidParentheses(char *s, int* returnSize)
 {
+    if (s == NULL || returnSize == NULL) {
+        return NULL;
+    }
+    return NULL;
+    *returnSize = 0;
+    char **result = (char**)calloc(MAX_COUNT, sizeof(char*));
 
+    return result;
 }
 
 int main()
 {
-	/************************
-	char a[] = "()())()"; // ["()()()", "(())()"]
-	char a[] = "(a)())()"; // ["(a)()()", "(a())()"]
-	char a[] = "")(""; // [""]  */
-	char a[] = "2147483647";
-	int target = 2147483647;
-	/*************************/
-	int returnSize = 0;
-	char **p = removeInvalidParentheses(a, &returnSize);
-	if (p != NULL) {
-		for (int i = 0; i < returnSize; i++) {
-			printf("%s\n", p[i]);
-			free(p[i]);
-		}
-		free(p);
-	}  
-	return 0;
+    /*************************/
+    char a[] = "()())()"; // ["()()()", "(())()"]
+    // char a[] = "(a)())()"; // ["(a)()()", "(a())()"]
+    // char a[] = ")("; // [""]  
+    int returnSize = 0;
+    char **p = removeInvalidParentheses(a, &returnSize);
+    if (p != NULL) {
+        for (int i = 0; i < returnSize; i++) {
+            printf("%s\n", p[i]);
+            free(p[i]);
+        }
+        free(p);
+    }
+    if (isValid(a)) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+    return 0;
 }
 
