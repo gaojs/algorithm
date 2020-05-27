62ee0490b2d9316eb0dda5a00325baffd6adb8a6
notebook <notebook@ni8.fun>
2020-05-27 23:21:12 +0800

    欧拉回路，不懂

--- leetcode/753. 破解保险箱.cpp
+++ leetcode/753. 破解保险箱.cpp
@@ -2,15 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <math.h>
+
+#define MAX_COUNT 10000 
+int visited[MAX_COUNT] = {0};
+#define MAX_LEN 100 
+char str[MAX_LEN] = {0};
+int len = 0;
+int P = 0;
+
+void dfs(int k, int i) 
+{
+    i -= i / P * P;
+    i *= 10;
+    for (int j = 0; j < k; ++j) {
+        int t = i + j;
+        if (!visited[t]) {
+            visited[t] = 1;
+            dfs(k, t);
+            str[len++] = j + '0';
+        }
+    }
+}
 
 char *crackSafe(int n, int k)
 {
-	return NULL;
+    memset(visited, 0, sizeof(visited));
+    memset(str, 0, sizeof(str));
+    P = pow(10, n - 1);
+    visited[0] = 1;
+    len = 0;
+    dfs(k, 0);
+    for (int i = 0; i < n; i++) {
+    	str[len++] = '0';    	
+	} 
+    return str;
 }
 
 int main()
 {
-	int n = 1, k = 2; // 01
+	// int n = 1, k = 2; // 01
 	int n = 2, k = 2; // 00110 10011 11001
 	printf("%s", crackSafe(n,k));
 	return 0;
