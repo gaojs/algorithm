affec1b99a363a38d6f1fd6872642aecb68bf821
gaojs <a@gaojs.cn>
2020-06-07 16:39:35 +0800

    快速排序，双指针法。时间复杂度，可以降低到O(nlogn)。

---
+++ leetcode/cpp/1. 两数之和3.cpp
@@ -0,0 +1,57 @@
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+struct node {
+	int v; // value
+	int i; // index
+	
+	node(int vt, int it) {
+		v=vt, i=it;
+	}
+}; 
+
+struct Cmp { // 用来排序的结构体
+	bool operator()(const node &a, const node &b) {
+		return a.v < b.v;
+	}	
+}; 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>&a, int target) {
+    	vector<node>ns;
+        for (int i = 0; i < a.size(); i++) {
+        	ns.emplace_back(a[i], i);
+		}
+		sort(ns.begin(), ns.end(), Cmp());
+		int left = 0, right = ns.size()-1;
+		while (left < right) {
+			int t = ns[left].v + ns[right].v;
+			if (t == target) {
+				return {ns[left].i, ns[right].i};
+			} else if (t < target) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return {0, 0} ;
+    }
+};
+
+int main () 
+{
+	vector<int>a = {2, 7, 11, 15};
+	int target = 9;
+	Solution s;
+	
+	vector<int> result = s.twoSum(a, target);
+	for(int i : result) {
+		cout<<i<<" ";
+	}    	
+} 
+    
