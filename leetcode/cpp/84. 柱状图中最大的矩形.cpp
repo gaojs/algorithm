86d66a142f737a835b0500ffe937ef61798db516
g30000454 <gaojinshan@huawei.com>
2020-07-25 22:32:45 +0800

    单调栈解决两题

---
+++ leetcode/cpp/84. 柱状图中最大的矩形.cpp
@@ -0,0 +1,44 @@
+﻿// 办公室熄灯
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights)
+{
+    int ret = 0;
+    stack<int> index;
+    index.push(-1); // 左边界起点
+    heights.push_back(0); // 右边界最小值
+    for (size_t i = 0; i < heights.size(); i++) {
+        while (index.size() > 1) {
+            int h = heights[index.top()];
+            if (h < heights[i]) {
+                break; // 直接入栈
+            }            
+            index.pop(); // 出栈
+            int left = index.top();
+            int w = (i - 1) - left;
+            int area = h * w;
+            ret = max(ret, area);
+        }
+        index.push(i); // 入栈
+    }
+    return ret;
+}
+
+int main(void)
+{
+    vector<int> heights = { 2,1,5,6,2,3 };
+    cout << largestRectangleArea(heights);
+    return 0;
+}
+
---
+++ leetcode/cpp/85. 最大矩形.cpp
@@ -0,0 +1,71 @@
+﻿// 办公室熄灯
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights)
+{
+    int ret = 0;
+    stack<int> index;
+    index.push(-1); // 左边界起点
+    heights.push_back(0); // 右边界最小值
+    for (size_t i = 0; i < heights.size(); i++) {
+        while (index.size() > 1) {
+            int h = heights[index.top()];
+            if (h < heights[i]) {
+                break; // 直接入栈
+            }
+            index.pop(); // 出栈
+            int left = index.top();
+            int w = (i - 1) - left;
+            int area = h * w;
+            ret = max(ret, area);
+        }
+        index.push(i); // 入栈
+    }
+    return ret;
+}
+
+int maximalRectangle(vector<vector<char>>& matrix)
+{
+    if (matrix.empty()) {
+        return 0;
+    }
+    int maxRect = 0;
+    vector<int> height(matrix[0].size(), 0);
+    for (size_t i = 0; i < matrix.size(); i++) {
+        for (size_t j = 0; j < height.size(); j++) {
+            if (matrix[i][j] == '0') {
+                height[j] = 0;
+            } else {
+                height[j]++;
+            }
+        }
+        int area = largestRectangleArea(height);
+        maxRect = max(maxRect, area);
+        height.pop_back();
+    }
+    return maxRect;
+}
+
+int main(void)
+{
+    vector< vector<char> > matrix = {
+        {'1','0','1','0','0'},
+        {'1','0','1','1','1'},
+        {'1','1','1','1','1'},
+        {'1','0','0','1','0'}
+    };
+    cout << maximalRectangle(matrix);
+    return 0;
+}
+
