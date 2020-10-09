e4a18c0b4473e56df8f72eb162e1f7671149b4e0
g30000454 <gaojinshan@huawei.com>
2020-10-09 08:43:48 +0800

    141. 环形链表

---
+++ leetcode/cpp/141. 环形链表.cpp
@@ -0,0 +1,32 @@
+#include <securec.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *p = head;
+    ListNode *q = head;
+    while(p!=nullptr && q!=nullptr){
+        p = p->next; //慢
+        q = q->next; //快
+        if (q == nullptr) {
+            return false;
+        } else {
+            q = q->next; //快
+        }
+        if (p == q) { //追上了
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+}
